04e74b6a943c3bf8ed4e9a171581582d1ce949a2
Gennon <imgennon@gmail.com>
2016-04-29 11:56:01 +0200

    Improved the Welcome page.

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,3 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { PageHeader, Jumbotron, Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
-export default () => <div>Welcome to our slice of paradise</div>;
+class Welcome extends Component {
+  render() {
+    return (
+      <div>
+        <PageHeader>Welcome <small>to our place</small></PageHeader>
+        <Jumbotron>
+          <h3>About</h3>
+          <p>This page will show how to do Redux Auth stuff. It even makes use of Firebase auth! How cool is that?!?</p>
+          <p>
+            <LinkContainer to="/signin">
+              <Button bsStyle="primary">Sign in</Button>
+            </LinkContainer>
+          </p>
+        </Jumbotron>    
+      </div>
+    );
+  }
+}
+
+export default Welcome;
